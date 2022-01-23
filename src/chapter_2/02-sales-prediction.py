71c1e768d5e154eb2b4fa0c830b1b58577e5c767
Luna Ramirez <cram8197@student.spcc.edu>
2022-01-23 17:01:01 -0500

    feat: finish exercise 2.2

---
+++ src/chapter_2/02-sales-prediction.py
@@ -0,0 +1,4 @@
+ANNUAL_PROFIT = 0.23
+projected_sales = int(input('Enter the projected amount of total sales: '))
+
+print(f'Currently we are expecting to gain ${projected_sales * ANNUAL_PROFIT:,.2f} this year.')
