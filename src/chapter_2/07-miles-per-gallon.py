0159ec3fc03da560069218903dd9a2d8e0339f18
Luna Ramirez <cram8197@student.spcc.edu>
2022-01-24 18:28:24 -0500

    feat: finish exercise 2.7

---
+++ src/chapter_2/07-miles-per-gallon.py
@@ -0,0 +1,5 @@
+miles_driven = float(input('Enter the miles driven: '))
+gallons_used = float(input('Enter the gallons of gas used: '))
+mpg = miles_driven / gallons_used
+
+print(f'Your car gets an average of {mpg:.2f}MPG')
