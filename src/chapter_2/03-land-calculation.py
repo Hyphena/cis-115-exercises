4a861015433f608da18a8ce5f3f722642bfbe66d
Luna Ramirez <cram8197@student.spcc.edu>
2022-01-23 21:47:35 -0500

    feat: finish exercise 2.3

---
+++ src/chapter_2/03-land-calculation.py
@@ -0,0 +1,4 @@
+ACRE_SQUARE_FEET = 43560
+sqFt = float(input('Enter the total square footage in your tract of land: '))
+
+print(f'Your tract contains {sqFt / ACRE_SQUARE_FEET:,.2f} acre(s) of land.')
